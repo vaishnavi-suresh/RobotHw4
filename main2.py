b68066a2ada1b13587ae1479d08d0835b6992592
Vaishnavi <vs2753@nyu.edu>
2024-11-09 11:47:11 -0500

    change waypoint configuration

--- main2.py
+++ main2.py
@@ -53,16 +53,16 @@ async def findWaypt(base,slam, arrPos):
     minDist = np.sqrt((y-arrPos[0][0])**2+(x-arrPos[0][1])**2)
     minIndex = 0
     for i, wp in enumerate(arrPos):
-        wpX = arrPos[i][0] *100
-        wpY = arrPos[i][1] *100
+        wpX = arrPos[i][0]
+        wpY = arrPos[i][1]
         wpTheta = arrPos[i][2]
         dist = np.sqrt((y-wpY)**2+(x-wpX)**2)
         if dist<minDist:
             minDist = dist
             minIndex = i
 
-    print(f'trying to go to: x= {arrPos[minIndex][0] *100}')
-    print(f'trying to go to: y= {arrPos[minIndex][1] *100}')
+    print(f'trying to go to: x= {arrPos[minIndex][0]}')
+    print(f'trying to go to: y= {arrPos[minIndex][1]}')
     print(f'trying to go to: theta= {arrPos[minIndex][2]}')
     return minIndex
 
@@ -74,8 +74,8 @@ async def goThroughPath(base,slam,wpIndex, posArr):
     pos = await get_position(slam)
     currX = pos.x
     currY = pos.y
-    wpX = posArr[wpIndex][0]*100
-    wpY = posArr[wpIndex][1]*100
+    wpX = posArr[wpIndex][0]
+    wpY = posArr[wpIndex][1]
     wpTheta = posArr[wpIndex][2]
     dist = getDist(currX,currY,wpX,wpY)
     if dist <40:
@@ -102,23 +102,28 @@ async def main():
 
     base = Base.from_robot(robot, 'viam_base')
     slam = SLAMClient.from_robot(robot, 'slam-1')  # Initialize SLAM
-
+    pos = await slam.get_position()
+    x = pos.x
+    y = pos.y
+    theta = pos.theta
+    base_origin_x = pos.x -1000
+    base_origin_y = pos.y +1000
 
 
     #get a set of waypoints to track and populate them
     wp = np.zeros((40,3))
     for i in range(10):
-        wp[i][0]=i
-        wp[i][2]=90
-        wp[i+10][0]=10
-        wp[i+10][1]=i
-        wp[i+10][2]=180
-        wp[i+20][0]=10
-        wp[i+20][1]=10-i
-        wp[i+20][2]=270
+        wp[i][0]=i*100 + base_origin_x
+        wp[i][2]=0 
+        wp[i+10][0]=1000 +base_origin_x
+        wp[i+10][1]=i*100 + base_origin_y
+        wp[i+10][2]=90
+        wp[i+20][1]=1000+base_origin_y
+        wp[i+20][0]=1000-i*100 + base_origin_x
+        wp[i+20][2]=180
         wp[i+30][0]=0
-        wp[i+30][1]=10-i
-        wp[i+30][2]=0
+        wp[i+30][1]=1000-i*100 + base_origin_y
+        wp[i+30][2]=270
     
     #get initial position
     pos = await slam.get_position()
