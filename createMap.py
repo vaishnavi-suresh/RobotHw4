7206a7b38dd4786b6a0e55cf223afd806d9f8432
Vaishnavi <vs2753@nyu.edu>
2024-11-10 18:49:20 -0500

    wrap async function

--- createMap.py
+++ createMap.py
@@ -1,15 +1,21 @@
+import asyncio
 from viam.services.slam import SLAMClient
 
+async def save_internal_state(robot):
+    # Create the SLAM service client
+    slam_service = SLAMClient.from_robot(robot=robot, name="my_slam_service")
+
+    # Retrieve the internal state
+    internal_state_chunks = await slam_service.get_internal_state()
+
+    # Concatenate the byte chunks
+    internal_state_data = b''.join(internal_state_chunks)
+
+    # Save to a file
+    with open('map_internal_state.pbstream', 'wb') as file:
+        file.write(internal_state_data)
+
+# Run the async function
 # Assume 'robot' is your connected RobotClient instance
-slam_service = SLAMClient.from_robot(robot=robot, name="my_slam_service")
-
-# Retrieve the internal state
-internal_state_chunks = await slam_service.get_internal_state()
-
-# Concatenate the byte chunks
-internal_state_data = b''.join(internal_state_chunks)
-
-# Save to a file
-with open('map_internal_state.pbstream', 'wb') as file:
-    file.write(internal_state_data)
+asyncio.run(save_internal_state(robot))
 
