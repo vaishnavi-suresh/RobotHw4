a0d607ea24de8c7d89a52bda7e9925c7218a69bf
Vaishnavi <vs2753@nyu.edu>
2024-11-12 23:28:57 -0500

    change correct func

--- main2.py
+++ main2.py
@@ -112,7 +112,7 @@ async def goThroughPath(orig,base,slam,wpIndex, posArr):
         pos = await slam.get_position()
         currX = pos.x
         currY = pos.y
-        if getDist(currX,currY,posArr[next][0],posArr[next][1])<200:
+        if getDist(currX,currY,posArr[next][0],posArr[next][1])<150:
             print("NOT CLOSEST")
                 
 
