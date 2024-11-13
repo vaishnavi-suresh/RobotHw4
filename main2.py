660115f5041fec0a2f4d469fd877c1f4171679f7
Vaishnavi <vs2753@nyu.edu>
2024-11-13 18:29:03 -0500

    remove normalization

--- main2.py
+++ main2.py
@@ -67,7 +67,7 @@ async def moveToPos(base, slam, x,y,theta):
         await moveAngle(base,slam,target_angle)
         await base.move_straight(int(dist/5),400)
         """
-    while dist >70:
+    while np.abs(x-currX)>20 or np.abs(y-currY)>20:
         currPos = await slam.get_position()
         currX = currPos.x
         currY = currPos.y
