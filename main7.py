c55239e1e9fcd26e316422315de994a242761213
naruminato1 <126989349+naruminato1@users.noreply.github.com>
2024-11-13 02:35:52 -0500

    20

--- main7.py
+++ main7.py
@@ -1,15 +1,13 @@
 
 
-# updated based off code from 5am code
+# main7.py
+
 
 import asyncio
-from viam.components.base import Base
+from viam.components.base import Base, Vector3
 from viam.robot.client import RobotClient
-from viam.rpc.dial import Credentials, DialOptions
 from viam.services.slam import SLAMClient
 from viam.services.motion import MotionClient
-import scipy
-from scipy.spatial import distance
 import numpy as np
 
 async def connect():
@@ -19,128 +17,116 @@ async def connect():
     )
     return await RobotClient.at_address('rover6-main.9883cqmu1w.viam.cloud', opts)
 
-async def get_position(slam):
-    position = await slam.get_position()
-    return position
+async def get_current_position(slam):
+    pos = await slam.get_position()
+    return pos.x, pos.y, pos.theta
 
-def getDist(currX, currY, wantX, wantY):
-    curr = (currX, currY)
-    want = (wantX, wantY)
-    return np.sqrt((wantX - currX)**2 + (wantY - currY)**2)
+def calculate_distance(x1, y1, x2, y2):
+    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-async def closestToPath(base, slam, arrPos):
-    wpIndex = await findWaypt(base, slam, arrPos)
-    baseX = arrPos[wpIndex][0]
-    baseY = arrPos[wpIndex][1]
-    baseTheta = arrPos[wpIndex][2]
-    await moveToPos(base, slam, baseX, baseY, baseTheta)
-    return wpIndex
+async def rotate_to_target(base, slam, target_x, target_y, current_x, current_y, current_theta):
+    # Calculate target angle in degrees
+    target_angle_rad = np.arctan2(target_y - current_y, target_x - current_x)
+    target_angle_deg = np.degrees(target_angle_rad)
+    
+    # Calculate the shortest rotation needed
+    rotation_needed = (target_angle_deg - current_theta + 180) % 360 - 180
+    
+    # Rotate in smaller increments for smoother motion
+    while abs(rotation_needed) > 2:  # 2 degree tolerance
+        rotation_step = np.clip(rotation_needed, -45, 45)
+        await base.spin(rotation_step, 45)
+        current_x, current_y, current_theta = await get_current_position(slam)
+        target_angle_rad = np.arctan2(target_y - current_y, target_x - current_x)
+        target_angle_deg = np.degrees(target_angle_rad)
+        rotation_needed = (target_angle_deg - current_theta + 180) % 360 - 180
 
-def getAngle(x, y):
-    arctan = np.arctan2(y, x)
-    return arctan
+async def move_to_waypoint(base, slam, target_x, target_y, target_theta, threshold_distance=50):
+    current_x, current_y, current_theta = await get_current_position(slam)
+    
+    # First rotate to face the target
+    await rotate_to_target(base, slam, target_x, target_y, current_x, current_y, current_theta)
+    
+    # Move straight to target
+    distance = calculate_distance(current_x, current_y, target_x, target_y)
+    if distance > threshold_distance:
+        await base.move_straight(int(distance), 400)
+    
+    # Final rotation to achieve target orientation
+    current_x, current_y, current_theta = await get_current_position(slam)
+    final_rotation = (target_theta - current_theta + 180) % 360 - 180
+    if abs(final_rotation) > 2:
+        await base.spin(final_rotation, 45)
 
-async def moveToPos(base, slam, x, y, theta):
-    print("move to point call")
-    currPos = await slam.get_position()
-    currX = currPos.x
-    currY = currPos.y
-    currTheta = currPos.theta
-    print(f'x={currX}')
-    print(f'y={currY}')
-    print(f'theta={currTheta}')
-    print(f'want x={x}')
-    print(f'want y={y}')
-    target_angle_rad = np.arctan2(y - currY, x - currX)
-    target_angle = np.degrees(target_angle_rad)
-    toMove = (target_angle - currTheta + 180) % 360 - 180
-    print(f'moving to angle: {toMove}')
-    dist = getDist(currX, currY, x, y)
-    await base.spin(toMove, 45)
-    await base.move_straight(int(dist), 100)
+async def find_closest_waypoint(slam, waypoints):
+    current_x, current_y, _ = await get_current_position(slam)
+    min_dist = float('inf')
+    closest_idx = 0
+    
+    for idx, (wx, wy, _) in enumerate(waypoints):
+        dist = calculate_distance(current_x, current_y, wx, wy)
+        if dist < min_dist:
+            min_dist = dist
+            closest_idx = idx
+    
+    return closest_idx
 
-async def findWaypt(base, slam, arrPos):
-    print("going to new position")
-    pos = await get_position(slam)
-    x = pos.x
-    y = pos.y
-    theta = pos.theta
-    print(f'currently at x = {x}')
-    print(f'currently at y = {y}')
-    print(f'currently at theta = {theta}')
-    minDist = np.sqrt((x - arrPos[0][0])**2 + (y - arrPos[0][1])**2)
-    minIndex = 0
-    for i, wp in enumerate(arrPos):
-        wpX = arrPos[i][0]
-        wpY = arrPos[i][1]
-        wpTheta = arrPos[i][2]
-        dist = np.sqrt((x - wpX)**2 + (y - wpY)**2)
-        if dist < minDist:
-            minDist = dist
-            minIndex = i
-
-    print(f'trying to go to: x= {arrPos[minIndex][0]}')
-    print(f'trying to go to: y= {arrPos[minIndex][1]}')
-    print(f'trying to go to: theta= {arrPos[minIndex][2]}')
-    return minIndex
-
-async def goThroughPath(base, slam, wpIndex, posArr):
-    while wpIndex < len(posArr):
-        next_wp = wpIndex + 1
-        if next_wp >= len(posArr):
-            next_wp = 0  # Optionally loop back to the first waypoint
-        c = await closestToPath(base, slam, posArr)
-        pos = await slam.get_position()
-        currX = pos.x
-        currY = pos.y
-        if getDist(currX, currY, posArr[wpIndex][0], posArr[wpIndex][1]) > 300:
-            print("NOT CLOSEST")
-            await moveToPos(base, slam, posArr[c][0], posArr[c][1], posArr[c][2])
-            wpIndex = c
-        else:
-            print("next wp")
-            print(next_wp)
-            await moveToPos(base, slam, posArr[next_wp][0], posArr[next_wp][1], posArr[next_wp][2])
-            wpIndex = next_wp
+async def follow_course(base, slam, waypoints, threshold_distance=50):
+    current_waypoint_idx = 0
+    
+    while True:
+        current_x, current_y, _ = await get_current_position(slam)
+        
+        # Check if we're off course and need to recover
+        closest_idx = await find_closest_waypoint(slam, waypoints)
+        current_waypoint = waypoints[closest_idx]
+        
+        # If we're too far from our current waypoint, go to the closest one
+        if calculate_distance(current_x, current_y, 
+                            current_waypoint[0], current_waypoint[1]) > threshold_distance:
+            print(f"Off course! Recovering to waypoint {closest_idx}")
+            current_waypoint_idx = closest_idx
+        
+        # Move to current waypoint
+        target = waypoints[current_waypoint_idx]
+        print(f"Moving to waypoint {current_waypoint_idx}: {target}")
+        await move_to_waypoint(base, slam, target[0], target[1], target[2], threshold_distance)
+        
+        # Check if we've reached the waypoint
+        current_x, current_y, _ = await get_current_position(slam)
+        if calculate_distance(current_x, current_y, target[0], target[1]) < threshold_distance:
+            current_waypoint_idx = (current_waypoint_idx + 1) % len(waypoints)
+        
+        await asyncio.sleep(0.1)
 
 async def main():
     robot = await connect()
     print('Resources:', robot.resource_names)
-
+    
     base = Base.from_robot(robot, 'viam_base')
-    slam = SLAMClient.from_robot(robot, 'slam-2')  # Initialize SLAM
-    motion = MotionClient.from_robot(robot, name="builtin")
-    pos = await slam.get_position()
-    x = pos.x
-    y = pos.y
-    theta = pos.theta
-    base_origin_x = x + 200
-    base_origin_y = y + 200
-
-    # Get a set of waypoints to track and populate them
-    wp = [
-        [0, 0, 0],
-        [1000, 0, 90],
-        [1000, 1000, 180],
-        [0, 1000, -90]
-    ]
-    for i in wp:
-        i[0] += base_origin_x
-        i[1] += base_origin_y
-
+    slam = SLAMClient.from_robot(robot, 'slam-2')
+    
     # Get initial position
     pos = await slam.get_position()
-    x = pos.x
-    y = pos.y
-    theta = pos.theta
-    print(f'currently at x={x}')
-    print(f'currently at y={y}')
-    print(f'currently at theta={theta}')
-
-    wpIndex = await closestToPath(base, slam, wp)
-
-    await goThroughPath(base, slam, wpIndex, wp)
-
+    base_origin_x = pos.x + 50
+    base_origin_y = pos.y + 50
+    
+    # Define square course waypoints [x, y, theta]
+    waypoints = [
+        [0, 0, 0],
+        [600, 0, 90],
+        [600, 600, 180],
+        [0, 600, -90]
+    ]
+    
+    # Adjust waypoints relative to starting position
+    for wp in waypoints:
+        wp[0] += base_origin_x
+        wp[1] += base_origin_y
+    
+    # Start following course
+    await follow_course(base, slam, waypoints)
+    
     await robot.close()
 
 if __name__ == '__main__':
